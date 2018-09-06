76ab0fbe18fabd3e589108cd55b2c5b5ea229bcc
Alex Galays <boubiyeah@hotmail.com>
2018-09-06 15:47:47 +0200

    fix: rollup watch mode

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from 'typescript'
 import typescriptPlugin from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
@@ -8,6 +9,7 @@ import pkg from './package.json'
 const isProd = process.env.BUILD === 'production'
 
 const plugins = [
+  nodeResolve(),
   commonjs({
     namedExports: { 'node_modules/winchan/winchan.js': ['open'] }
   }),
@@ -27,6 +29,13 @@ export default [
     ],
     plugins,
     onwarn: message => {
+      // tsc generates local polyfills such as __extends with 'this' referenced
+      if (message.code === 'THIS_IS_UNDEFINED') return
+
+      // space-lift Option depends on Either (for its toEither method) and Either depends on Option (toOption)
+      // No great way around that I'm afraid.
+      if (message.code === 'CIRCULAR_DEPENDENCY' && message.importer === 'node_modules/space-lift/es/option/index.js') return
+
       // We could list all the external modules to remove these warnings... but listing lodash-es
       // doesn't include all its sub-modules such as lodash-es/pickBy, etc so
       // it becomes incredibly tedious.
