683ec192e1ed4cc8f3e3f1f46028737a153eca01
Jean-Christophe Delmas <jc.delmas@gmail.com>
2018-10-24 10:44:02 +0200

    fix: type declaration

--- src/main/authResult.ts
+++ src/main/authResult.ts
@@ -5,10 +5,11 @@ import { idTokenPayload } from '../lib/jwt'
 export const authResult = v.object({
   accessToken: v.optional(v.string),
   expiresIn: v.optional(v.number),
+  tokenType: v.optional(v.literal('Bearer')),
   idToken: v.optional(v.string),
   idTokenPayload: v.optional(idTokenPayload),
   code: v.optional(v.string),
-  error: v.optional(v.string)
+  state: v.optional(v.string)
 })
 
 export type AuthResult = typeof authResult.T
