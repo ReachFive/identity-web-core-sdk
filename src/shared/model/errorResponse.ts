58195930a27c79aee16ab4c52dfdd8af32b3b955
Jean-Christophe Delmas <jc.delmas@gmail.com>
2018-12-11 15:11:27 +0100

    feat: enrich error type

--- src/shared/model/errorResponse.ts
+++ src/shared/model/errorResponse.ts
@@ -1,6 +1,14 @@
 export type ErrorResponse = {
   error: string
   errorDescription?: string
+  errorUserMsg?: string
+  errorDetails?: FieldError[]
+}
+
+export type FieldError = {
+  field: string
+  message: string
+  code: 'missing' | 'invalid'
 }
 
 export namespace ErrorResponse {
