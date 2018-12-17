ddb7a68e7da199bbcd046ce27e98f446efdadf3f
Jean-Christophe Delmas <jc.delmas@gmail.com>
2018-12-17 11:20:07 +0100

    add minification to umd bundle

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from 'typescript'
 import typescriptPlugin from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
+import { uglify } from 'rollup-plugin-uglify'
 import pkg from './package.json'
 
 
@@ -23,11 +24,11 @@ function isNpmDependency(name) {
   return /lodash-es/.test(name)
 }
 
-function createBundle({ file, format, name, external }) {
+function createBundle({ file, format, name, external, withUglify = false }) {
   return {
 		input: 'src/main/main.ts',
 		output: { file, format, name },
-    plugins,
+    plugins: withUglify ? [uglify(), ...plugins] : plugins,
     external,
     onwarn: message => {
       // tsc generates local polyfills such as __extends with 'this' referenced
@@ -43,7 +44,7 @@ function createBundle({ file, format, name, external }) {
 }
 
 export default [
-  createBundle({ file: 'umd/main.js', format: 'umd', name: 'reach5' }),
+  createBundle({ file: 'umd/main.js', format: 'umd', name: 'reach5', withUglify: true }),
   createBundle({ file: pkg.main, format: 'cjs', external: isNpmDependency }),
   createBundle({ file: pkg.module, format: 'es', external: isNpmDependency })
 ]
