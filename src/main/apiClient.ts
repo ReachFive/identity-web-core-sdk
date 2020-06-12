3ed333b18482df706134e100b57730026d6db953
Roxane Macé <roxane.mace@outlook.com>
2020-06-12 11:26:56 +0200

    Fix tslint errors

--- src/main/apiClient.ts
+++ src/main/apiClient.ts
@@ -14,7 +14,7 @@ import { UrlParser } from './urlParser'
 import { popupSize } from './providerPopupSize'
 import { createHttpClient, HttpClient } from './httpClient'
 import { computePkceParams, PkceParams } from './pkceService'
-import { 
+import {
   encodePublicKeyCredentialCreationOptions, encodePublicKeyCredentialRequestOptions,
   serializeRegistrationPublicKeyCredential, serializeAuthenticationPublicKeyCredential,
   CredentialCreationOptionsSerialized, CredentialRequestOptionsSerialized,
