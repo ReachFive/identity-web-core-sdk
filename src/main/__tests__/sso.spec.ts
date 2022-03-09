5265ce918a52539c0383cfd9a798243c04329281
Stanislas Zarka <stanislas.zarka@gmail.com>
2022-03-09 17:18:53 +0100

    Try test timeout

---
+++ src/main/__tests__/sso.spec.ts
@@ -0,0 +1,15 @@
+import { createDefaultTestClient } from './helpers/clientFactory'
+
+describe('SSO feature check', () => {
+  const { client } = createDefaultTestClient({ sso: false })
+
+  test('checkSession', async () => {
+    await expect(client.checkSession())
+      .rejects.toThrow(new Error(`Cannot call 'checkSession' if SSO is not enabled.`))
+  })
+
+  test('loginFromSession', async () => {
+    await expect(client.loginFromSession())
+      .rejects.toThrow(new Error(`Cannot call 'loginFromSession' if SSO is not enabled.`))
+  })
+})
