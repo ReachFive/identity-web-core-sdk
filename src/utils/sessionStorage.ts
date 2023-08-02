980297578a4b7b599905a9ae4c85da0c56716f97
sacha <sacha.memmi@reach5.co>
2023-08-02 17:07:27 +0200

    session storage

---
+++ src/utils/sessionStorage.ts
@@ -0,0 +1,25 @@
+export function getWithExpiry(key: string) {
+  const storedValue = sessionStorage.getItem(key)
+  if(!storedValue) {
+    return null
+  }
+  try {
+    const item = JSON.parse(storedValue)
+    const now = new Date()
+    if (now.getTime() > item.expiry) {
+      localStorage.removeItem(key)
+      return null
+    }
+    return item.value
+  } catch (e) {
+    return null
+  }
+}
+export function setWithExpiry(key: string, value: string, ttl: number) {
+  const now = new Date()
+  const item = {
+    value,
+    expiry: now.getTime() + ttl
+  }
+  sessionStorage.setItem(key, JSON.stringify(item))
+}
