e768e9ad997739d144b05e0c3a1d7b86a108fccd
Quentin AUPETIT <quentin.aupetit@reach5.co>
2023-11-14 13:16:36 -0400

    add IdTokenPayload type missing properties

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,21 +3,46 @@ import { decodeBase64UrlSafe } from './base64'
 
 export type Gender = 'female' | 'male' | 'other'
 
+export type Address = {
+  formatted?: string
+  streetAddress?: string
+  locality?: string
+  region?: string
+  postalCode?: string
+  country?: string
+}
+
 export interface IdTokenPayload {
+  acr?: string
+  address?: Address
+  amr?: string[]
+  atHash?: string
+  aud?: string[]
+  authTime?: number
   authType?: string
+  azp?: string
   birthdate?: string
+  customFields?: Record<string, any>
+  customIdentifier?: string
   email?: string
   emailVerified?: boolean
   exp?: number
+  externalId?: string
   familyName?: string
   gender?: Gender
   givenName?: string
   iat?: number
   iss?: string
   locale?: string
+  middleName?: string
   name?: string
   newUser?: boolean
+  nickname?: string
+  nonce?: string
+  phoneNumber?: string
+  phoneNumberVerified?: boolean
   picture?: string
+  preferredUsername?: string
   profile?: string
   sub?: string
   updatedAt?: string
