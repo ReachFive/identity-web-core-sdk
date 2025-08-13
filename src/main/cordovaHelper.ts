fc2689896feb016bbf60bc9a38acab94df7069b2
Quentin Aupetit <qaupetit@gmail.com>
2025-08-13 13:24:30 +0200

    tiny refacto on url parsing (#269)

--- src/main/cordovaHelper.ts
+++ src/main/cordovaHelper.ts
@@ -1,16 +1,16 @@
 import { UrlParser } from './urlParser'
 
 export function initCordovaCallbackIfNecessary(urlParser: UrlParser): void {
-  if (!window.cordova) return
+  if (typeof window === 'undefined' || !('cordova' in window)) return
   if (window.handleOpenURL) return
 
-  window.handleOpenURL = url => {
+  window.handleOpenURL = (url) => {
     const cordova = window.cordova
     if (!cordova) return
 
     const parsed = urlParser.checkUrlFragment(url)
 
-    if (parsed && cordova.plugins && cordova.plugins.browsertab) {
+    if (parsed && cordova?.plugins?.browsertab) {
       cordova.plugins.browsertab.close()
     }
   }
